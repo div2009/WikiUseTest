54d7349d1d3071306d5244cd65fb112e1e10bc87
div2009 <div2009@foxmail.com>
2022-07-22 14:26:00 +0800

    tes1

--- test.lua
+++ test.lua
@@ -1,13 +1,19 @@
 print("hello world");
 local function printtable(tab)
+    
+    print("table = {")
     for k, v in pairs(tab) do
         local k_type = type(k)
         local v_type = type(v)
-        print("k_type=",k_type)
-        print("v_type=",v_type)
+
+       -- print("k_type=",k_type)
+       -- print("v_type=",v_type)
+
         local key = (k_type == "string" and   k  ) or (k_type == "number" and " ")
+
         local value = (v_type == "table" and printtable(v)) or (v_type == "boolean" and tostring(v)) or (v_type == "string" and   v  ) or (v_type == "number" and v)
-        local str = string.format('%s = %s\n',key,value)
+
+        local str = (k_type == "number" and string.format('  {%s},',value)) or string.format('  [%s] = {%s},',key,value)
         print(str)
         
 
@@ -16,18 +22,27 @@ local function printtable(tab)
 
 
     end
+    print("}")
 
 end
+
+
+local function Max(tab)
+    for k,v in pairs(tab) do
+    
+    end
+end
+
 
 local function test(A)
 
     --print(A);
     local a = "ACG"
     local i = 100
-    local tab = { "ggg" }
+    local tab = { "ggg",11,2,3,4 ,"试试"}   
 
-    local t = (a == "ACG" and "\"" .. i .. "\":") or (a == "number" and "")
-    tab[#tab + 1] = a and i and tostring(t) .. "xxxx" or nil
+   -- local t = (a == "ACG" and "\"" .. i .. "\":") or (a == "number" and "")
+   -- tab[#tab + 1] = a and i and tostring(t) .. "xxxx" or nil
 
     -- print(t)
     printtable(tab)
